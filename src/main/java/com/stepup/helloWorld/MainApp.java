a121c050eac082211331fe002aa175821af20ccf
ABalykova <asbalykova@dev.vtb.ru>
2023-05-06 22:33:43 +0400

    add new welcome

--- src/main/java/com/stepup/helloWorld/MainApp.java
+++ src/main/java/com/stepup/helloWorld/MainApp.java
@@ -3,6 +3,6 @@ package com.stepup.helloWorld;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello my team)");
+        System.out.println("Hello my team");
     }
 }
